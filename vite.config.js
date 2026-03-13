9938b4dde90803dca8ff5b4907f407b9673bb5af
Ganesh <pwork11421@gmail.com>
2026-03-13 17:52:47 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   base: '/',
-  server: {
-    allowedHosts: ['0da5-103-21-78-185.ngrok-free.app'],
-  },
-});
+  build: {
+    chunkSizeWarningLimit: 1000
+  }
+})
